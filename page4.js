7b9b5b6e95e4a8384becaa95ba81f2a90005f16f
koza-bit <56552912+koza-bit@users.noreply.github.com>
2019-11-24 11:13:57 +0100

    Update page4.js

--- page4.js
+++ page4.js
@@ -1,239 +1,173 @@
-window.onload = function() {
-  let c = init("canvas").c,
-    canvas = init("canvas").canvas,
-    w = (canvas.width = window.innerWidth),
-    h = (canvas.height = window.innerHeight),
-    mouse = { x: false, y: false },
-    last_mouse = {};
+var canvas = document.createElement("canvas"),
+  c = canvas.getContext("2d");
+var w = (canvas.width = window.innerWidth),
+  h = (canvas.height = window.innerHeight);
 
-  //initiation
+document.body.appendChild(canvas);
+var particles,
+  t = 1,
+  mr = 10;
 
-  function dist(p1x, p1y, p2x, p2y) {
-    return Math.sqrt(Math.pow(p2x - p1x, 2) + Math.pow(p2y - p1y, 2));
+class particle {
+  constructor(x, y, s) {
+    this.x = x;
+    this.y = y;
+    this.pos = {
+      x: this.x,
+      y: this.y
+    };
+    this.s = s;
+    this.col = Math.floor(Math.random() * 255);
+    this.ang = Math.atan2(h / 2 - this.y, w / 2 - this.x) + 3 * Math.PI / 4;
+    this.vx = 2 * Math.cos(this.ang);
+    this.vy = 2 * Math.sin(this.ang);
+    this.ax = 0;
+    this.ay = 0;
+    this.hist = [];
   }
 
-  class segment {
-    constructor(parent, l, a, first) {
-      this.first = first;
-      if (first) {
-        this.pos = {
-          x: parent.x,
-          y: parent.y
-        };
-      } else {
-        this.pos = {
-          x: parent.nextPos.x,
-          y: parent.nextPos.y
-        };
+  move(xt, yt) {
+    this.tx = xt;
+    this.ty = yt;
+    //distance from target
+    this.dist = Math.sqrt(Math.pow(this.x - xt, 2) + Math.pow(this.y - yt, 2));
+    //gravitational acceleration
+    this.mg = 100;
+    //gravity force angle
+    this.ang = Math.atan2(yt - this.y, xt - this.x);
+    //gravity force strenght
+    this.force = this.mg * this.s * mr / Math.pow(this.dist, 2);
+    //acceleration
+    this.ax = this.force * Math.cos(this.ang);
+    this.ay = this.force * Math.sin(this.ang);
+    if (this.dist > mr + this.s) {
+      //velocity
+      this.vx += 0.9 * this.ax * t;
+      this.vy += 0.9 * this.ay * t;
+      //friction
+      this.vx *= 0.9;
+      this.vy *= 0.9;
+      //movement
+      this.x += this.vx * t;
+      this.y += this.vy * t;
+    } else {
+      //what happens behind event horizon
+      this.x = Math.random() * (w + 20) - 10;
+      this.y = Math.random() * (h + 20) - 10;
+      while (this.x > 0 && this.x < w && this.y > 0 && this.y < h) {
+        this.x = Math.random() * (w + 20) - 10;
+        this.y = Math.random() * (h + 20) - 10;
       }
-      this.l = l;
-      this.ang = a;
-      this.nextPos = {
-        x: this.pos.x + this.l * Math.cos(this.ang),
-        y: this.pos.y + this.l * Math.sin(this.ang)
-      };
+      this.ang = Math.atan2(h / 2 - this.y, w / 2 - this.x) + 3 * Math.PI / 4;
+      this.vx = 1 * Math.cos(this.ang);
+      this.vy = 1 * Math.sin(this.ang);
+      this.ax = 0;
+      this.ay = 0;
+      this.s = Math.pow(Math.random() * 1.1 + 0.1, 4) + 1;
+      //black hole gaining some mass
+      mr = Math.pow(Math.pow(mr, 2) + Math.pow(this.s, 2), 1 / 2);
+      this.hist.splice(0, this.hist.length);
     }
-    update(t) {
-      this.ang = Math.atan2(t.y - this.pos.y, t.x - this.pos.x);
-      this.pos.x = t.x + this.l * Math.cos(this.ang - Math.PI);
-      this.pos.y = t.y + this.l * Math.sin(this.ang - Math.PI);
-      this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
-      this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
-    }
-    fallback(t) {
-      this.pos.x = t.x;
-      this.pos.y = t.y;
-      this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
-      this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
-    }
-    show() {
-      c.lineTo(this.nextPos.x, this.nextPos.y);
-    }
-  }
-
-  class tentacle {
-    constructor(x, y, l, n, a) {
-      this.x = x;
-      this.y = y;
-      this.l = l;
-      this.n = n;
-      this.t = {};
-      this.rand = Math.random();
-      this.segments = [new segment(this, this.l / this.n, 0, true)];
-      for (let i = 1; i < this.n; i++) {
-        this.segments.push(
-          new segment(this.segments[i - 1], this.l / this.n, 0, false)
-        );
-      }
-    }
-    move(last_target, target) {
-      this.angle = Math.atan2(target.y-this.y,target.x-this.x);
-      this.dt = dist(last_target.x, last_target.y, target.x, target.y)+5;
-      this.t = {
-        x: target.x - 0.8*this.dt*Math.cos(this.angle),
-        y: target.y - 0.8*this.dt*Math.sin(this.angle)
-      };
-      if(this.t.x){
-        this.segments[this.n - 1].update(this.t);
-      }else{
-        this.segments[this.n - 1].update(target);
-      }
-      for (let i = this.n - 2; i >= 0; i--) {
-        this.segments[i].update(this.segments[i + 1].pos);
-      }
-      if (
-        dist(this.x, this.y, target.x, target.y) <=
-        this.l + dist(last_target.x, last_target.y, target.x, target.y)
-      ) {
-        this.segments[0].fallback({ x: this.x, y: this.y });
-        for (let i = 1; i < this.n; i++) {
-          this.segments[i].fallback(this.segments[i - 1].nextPos);
-        }
-      }
-    }
-    show(target) {
-      if (dist(this.x, this.y, target.x, target.y) <= this.l) {
-        c.globalCompositeOperation = "color-dodge";
-        c.beginPath();
-        c.lineTo(this.x, this.y);
-        for (let i = 0; i < this.n; i++) {
-          this.segments[i].show();
-        }
-        c.strokeStyle = "hsl("+(this.rand*60+180)+",100%," + (this.rand * 60 + 25) + "%)";
-        c.lineWidth = this.rand * 2;
-        c.lineCap="round";
-        c.lineJoin="round";
-        c.stroke();
-        c.globalCompositeOperation = "source-over";
-      }
-    }
-    show2(target) {
-      c.beginPath();
-      if (dist(this.x, this.y, target.x, target.y) <= this.l) {
-        c.arc(this.x, this.y, 2*this.rand+1, 0, 2 * Math.PI);
-        c.fillStyle = "white";
-      } else {
-        c.arc(this.x, this.y, this.rand*2, 0, 2 * Math.PI);
-        c.fillStyle = "darkcyan";
-      }
-      c.fill();
+    //tails
+    this.pos = {
+      x: this.x,
+      y: this.y
+    };
+    this.hist.push(this.pos);
+    if (this.hist.length > 5) {
+      this.hist.splice(0, 1);
     }
   }
 
-  let maxl = 300,
-    minl = 50,
-    n = 30,
-    numt = 500,
-    tent = [],
-    clicked = false,
-    target = { x: 0, y: 0 },
-    last_target = {},
-    t = 0,
-    q = 10;
-
-  for (let i = 0; i < numt; i++) {
-    tent.push(
-      new tentacle(
+  show() {
+    //showing tails
+    c.beginPath();
+    this.hist.forEach(hi => c.lineTo(hi.x, hi.y));
+    c.lineWidth = 2 * this.s;
+    c.lineJoin = "round";
+    c.lineCap = "round";
+    c.strokeStyle = "rgba(255,255,255,0.1)";
+    c.stroke();
+    //showing bodies
+    c.beginPath();
+    c.arc(this.x, this.y, this.s, 0, 2 * Math.PI);
+    c.fillStyle = "white";
+    c.fill();
+    //showing black hole
+    c.beginPath();
+    c.arc(this.tx, this.ty, mr, 0, 2 * Math.PI);
+    c.fillStyle = "black";
+    c.fill();
+  }
+}
+// universe creation
+particles = Array(300)
+  .fill()
+  .map(
+    p =>
+      new particle(
         Math.random() * w,
         Math.random() * h,
-        Math.random() * (maxl - minl) + minl,
-        n,
-        Math.random() * 2 * Math.PI
+        Math.pow(Math.random() * 1.1 + 0.1, 4) + 1
       )
-    );
-  }
-  function draw() {
-    //animation
-    if (mouse.x) {
-      target.errx = mouse.x - target.x;
-      target.erry = mouse.y - target.y;
-    } else {
-      target.errx =
-        w / 2 +
-        (h / 2 - q) *
-          Math.sqrt(2) *
-          Math.cos(t) /
-          (Math.pow(Math.sin(t), 2) + 1) -
-        target.x;
-      target.erry =
-        h / 2 +
-        (h / 2 - q) *
-          Math.sqrt(2) *
-          Math.cos(t) *
-          Math.sin(t) /
-          (Math.pow(Math.sin(t), 2) + 1) -
-        target.y;
-    }
-
-    target.x += target.errx / 10;
-    target.y += target.erry / 10;
-
-    t += 0.01;
-    
-    c.beginPath();
-    c.arc(target.x, target.y, dist(last_target.x, last_target.y, target.x, target.y)+5, 0, 2 * Math.PI);
-    c.fillStyle = "hsl(210,100%,80%)";
-    c.fill();
-
-    for (i = 0; i < numt; i++) {
-      tent[i].move(last_target, target);
-      tent[i].show2(target);
-    }
-    for (i = 0; i < numt; i++) {
-      tent[i].show(target);
-    }
-    last_target.x = target.x;
-    last_target.y = target.y;
-  }
-
-  canvas.addEventListener(
-    "mousemove",
-    function(e) {
-      last_mouse.x = mouse.x;
-      last_mouse.y = mouse.y;
-
-      mouse.x = e.pageX - this.offsetLeft;
-      mouse.y = e.pageY - this.offsetTop;
-    },
-    false
   );
-
-  canvas.addEventListener("mouseleave", function(e) {
-    mouse.x = false;
-    mouse.y = false;
-  });
-
-  canvas.addEventListener(
-    "mousedown",
-    function(e) {
-      clicked = true;
-    },
-    false
-  );
-
-  canvas.addEventListener(
-    "mouseup",
-    function(e) {
-      clicked = false;
-    },
-    false
-  );
-
-  function loop() {
-    window.requestAnimFrame(loop);
-    // c.fillStyle="rgba(30,30,30,0.1)";
-    // c.fillRect(0, 0, w, h);
-    c.clearRect(0, 0, w, h);
-    draw();
-  }
-
-  window.addEventListener("resize", function() {
-    (w = canvas.width = window.innerWidth),
-      (h = canvas.height = window.innerHeight);
-    loop();
-  });
-
-  loop();
-  setInterval(loop, 1000 / 60);
+var i = 0;
+//there time begins
+function draw() {
+  particles.forEach(p => p.move(mouse.x, mouse.y));
+  particles.forEach(p => p.show());
+}
+//controlling black hole
+var mouse = {
+  x: w / 2,
+  y: h / 2
 };
+var last_mouse = {
+  x: 0,
+  y: 0
+};
+
+canvas.addEventListener(
+  "mousemove",
+  function(e) {
+    last_mouse.x = mouse.x;
+    last_mouse.y = mouse.y;
+
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+  },
+  false
+);
+
+window.requestAnimFrame = (function() {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
+
+function loop() {
+  setTimeout(function() {
+    window.requestAnimFrame(loop);
+    c.fillStyle = "rgba(30,30,30,1)";
+    c.fillRect(0, 0, w, h);
+    draw();
+  }, 1000 / 60);
+}
+
+window.addEventListener("resize", function() {
+  (w = canvas.width = window.innerWidth),
+    (h = canvas.height = window.innerHeight);
+  c.fillStyle = "rgba(30,30,30,1)";
+  c.fillRect(0, 0, w, h);
+});
+
+loop();
+
 
